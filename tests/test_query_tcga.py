63fc44ae8cf997328a72466f91c5b711ab44b00d
Jacqueline Buros Novik <jackinovik@gmail.com>
2016-09-14 15:53:18 -0400

    add extra lines; add test cases for clinical data

--- tests/test_query_tcga.py
+++ tests/test_query_tcga.py
@@ -2,6 +2,8 @@
 from query_tcga import query_tcga as qt
 import pytest
 import shutil
+import pandas as pd
+
 
 def test_construct_filter_parameters():
    res = qt._construct_filter_parameters(project_name='TCGA-BLCA', data_category='Clinical')
@@ -12,15 +14,18 @@ def test_construct_filter_parameters():
         'op': 'and'}
    assert res == expects
 
+
 def test_convert_to_list():
     assert qt._convert_to_list('Clinical') == ['Clinical']
     assert qt._convert_to_list(['Clinical']) == ['Clinical']
     assert qt._convert_to_list(('Clinical','Biospecimen')) == ['Clinical', 'Biospecimen']
 
+
 def test_construct_parameters():
     ## minimal testing because dictionary (which is converted to string) 
     ## isn't always in same order. *could* sort the dict, but prob isn't necessary
     assert list(qt._construct_parameters(project_name='TCGA-BLCA', size=5).keys()).sort() == ['filters','size'].sort()
+
 
 def test_list_valid_fields():
     expected = ['files.access', 'files.acl', 'files.analysis.analysis_id']
@@ -28,6 +33,7 @@ def test_list_valid_fields():
     res.sort()
     assert res[0:3] == expected
     assert len(res) >= 200
+
 
 def test_list_valid_options():
     expected = ['Simple Nucleotide Variation',
@@ -44,15 +50,18 @@ def test_list_valid_options():
     with pytest.raises(ValueError):
         qt._list_valid_options('files.data_category', endpoint_name='files', strip_endpoint_from_field_name=False)
 
+
 def test_verify_field_name():
     assert qt._verify_field_name(field_name='files.data_category', endpoint_name='files') == True
     with pytest.raises(ValueError):
         qt._verify_field_name(field_name='data_category', endpoint_name='files')
 
+
 def test_verify_field_values():
     assert qt._verify_field_values(['Clinical'], field_name='files.data_category', endpoint_name='files') == True  
     with pytest.raises(ValueError):
         qt._verify_field_values(['Clinical'], field_name='data_category', endpoint_name='files')
+
 
 def test_verify_data_list():
     assert qt._verify_data_list(['Clinical'], allowed_values=['Clinical', 'Biospecimen']) == True
@@ -60,12 +69,15 @@ def test_verify_data_list():
     with pytest.raises(ValueError):
         qt._verify_data_list(['TCGA-BLCA'], allowed_values=['Clinical'])
 
+
 def test_get_num_pages():
     assert isinstance(qt._get_num_pages(project_name='TCGA-BLCA', data_category=['Clinical'], size=5), int)
+
 
 def test_get_manifest_once():
     response = qt._get_manifest_once(project_name='TCGA-BLCA', data_category=['Clinical'], size=5)
     assert response.status_code == 200
+
 
 def test_get_manifest():
     res = qt.get_manifest(project_name='TCGA-BLCA', data_category=['Clinical'], pages=2, size=2)
@@ -80,6 +92,22 @@ def test_download_files():
     res = qt._download_files(project_name='TCGA-BLCA', data_category='Clinical', max_pages=1, page_size=5, data_dir='tests/test_data')
     assert isinstance(res, list)
     assert len(res) == 5
+
+
+def test_download_clinical_files():
+    shutil.rmtree('tests/test_data')
+    res = qt.download_clinical_files(project_name='TCGA-BLCA', max_pages=1, page_size=5, data_dir='tests/test_data')
+    assert isinstance(res, list)
+    assert len(res) == 5
+
+
+def test_get_clinical_data():
+    res = qt.get_clinical_data(project_name='TCGA-BLCA', max_pages=1, page_size=5, data_dir='tests/test_data')
+    assert isinstance(res, pd.DataFrame)
+    assert len(res.index) == 5
+    assert '_source_type' in res.columns
+    assert '_source_desc' in res.columns
+    assert 'patient_id' in res.columns
 
 
 

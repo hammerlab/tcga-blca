7d1961d5c9bab1c023ae0343475574498e4457a5
Jacqueline Buros Novik <jackinovik@gmail.com>
2016-09-16 18:00:49 -0400

    add project name to printout

--- refresh_clinical_data.py
+++ refresh_clinical_data.py
@@ -1,4 +1,7 @@
 from query_tcga import query_tcga as qt
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 def refresh_clin_data(project_name, filename):
 	clin = qt.get_clinical_data(project_name=project_name)
@@ -6,5 +9,5 @@ def refresh_clin_data(project_name, filename):
 
 if __name__ == '__main__':
 	refresh_clin_data(project_name='TCGA-BLCA', filename='data/clinical.csv')
-	print("Clinical data written to {}".format('data/clinical.csv'))
+	print("Clinical data written to {file} for project {project}".format(file='data/clinical.csv', project='TCGA-BLCA'))
 

2726a5c0ba0bc52ecba1ee263178252b7a816f33
Jacqueline Buros Novik <jackinovik@gmail.com>
2016-09-19 13:15:03 -0400

    rename wxs_samples to samples

--- refresh_vcf_data.py
+++ refresh_vcf_data.py
@@ -1,7 +1,7 @@
-from query_tcga import query_tcga as qt
+from query_tcga import samples
 
 def refresh_vcf_data(project_name):
-	files = qt.download_vcf_data(project_name=project_name, data_dir='data/gdc', n=1)
+	files = samples.download_vcf_data(project_name=project_name, data_dir='data/gdc', n=1)
 
 if __name__ == '__main__':
 	refresh_vcf_data('TCGA-BLCA')

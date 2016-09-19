b584bb82a0a2c4f949d71137f638cf8deaa38922
Jacqueline Buros Novik <jackinovik@gmail.com>
2016-09-19 15:19:26 -0400

    remove n param, which was used for testing

--- refresh_vcf_data.py
+++ refresh_vcf_data.py
@@ -1,7 +1,7 @@
 from query_tcga import samples
 
 def refresh_vcf_data(project_name):
-	samples.download_vcf_files(project_name=project_name, data_dir='data/gdc', n=1)
+	samples.download_vcf_files(project_name=project_name, data_dir='data/gdc')
 
 if __name__ == '__main__':
 	refresh_vcf_data('TCGA-BLCA')

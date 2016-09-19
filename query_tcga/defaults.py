a6502955ebc77e4153126a4c47537a7c1d9d7a14
Jacqueline Buros Novik <jackinovik@gmail.com>
2016-09-19 17:10:23 -0400

    add analysis_id & sample_id to fileinfo metadata

--- query_tcga/defaults.py
+++ query_tcga/defaults.py
@@ -23,4 +23,5 @@ VALID_ENDPOINTS = ['files', 'projects', 'cases', 'annotations']
 # number of records per page, by default
 DEFAULT_SIZE = 500
 # fields to pull for 'file-metadata' table
-DEFAULT_FILE_FIELDS=['file_id','file_name','cases.submitter_id','cases.case_id','data_category','data_type','cases.samples.tumor_descriptor','cases.samples.tissue_type','cases.samples.sample_type','cases.samples.submitter_id','cases.samples.sample_id']
+DEFAULT_FILE_FIELDS=['file_id','file_name','cases.submitter_id','cases.case_id','data_category','data_type','cases.samples.tumor_descriptor','cases.samples.tissue_type','cases.samples.sample_type','cases.samples.submitter_id','cases.samples.sample_id',
+					'analysis.analysis_id']

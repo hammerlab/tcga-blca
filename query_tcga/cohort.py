a546ddcaff48eb5c21a582c257719d3b7ab10c17
Jacqueline Buros Novik <jackinovik@gmail.com>
2016-09-24 13:19:39 -0400

    pull vcfs fileinfo in bulk if n not specified

--- query_tcga/cohort.py
+++ query_tcga/cohort.py
@@ -1,11 +1,11 @@
 import cohorts
 from query_tcga import query_tcga as qt
 from query_tcga import samples
-from query_tcga import defaults
+from query_tcga import defaults, helpers
 import numpy as np
 import pandas as pd
 
-def prep_patient_data(row, snv_vcf_paths = None, **kwargs):
+def prep_patient_data(row, snv_vcf_paths=None, **kwargs):
     patient_id = row['case_id']
     deceased = row['vital_status'] != 'Alive'
     progressed = row['treatment_outcome_at_tcga_followup'] != 'Complete Response'
@@ -57,19 +57,35 @@ def prep_patient_data(row, snv_vcf_paths = None, **kwargs):
         pfs=pfs,
         benefit=benefit,
         additional_data=row,
-        snv_vcf_paths = snv_vcf_paths,
+        snv_vcf_paths=snv_vcf_paths,
     )
     return(patient)
 
 
-def build_cohort(project_name, include_vcfs=True, **kwargs):
+def _merge_filepath_with_fileinfo(files):
+    """ Given list of filepaths & fileinfo, add a field "filepath" to fileinfo & return it
+    """ 
+    filepath_data = pd.DataFrame(dict(file_id=helpers.convert_to_file_id(files), file_path=list(files)))
+    fileinfo = files.fileinfo
+    return pd.merge(fileinfo, filepath_data, on='file_id')
 
-    clin = qt.get_clinical_data(project_name=project_name, **kwargs)
 
+
+
+def build_cohort(project_name, include_vcfs=True, n=None, **kwargs):
+
+    clin = qt.get_clinical_data(project_name=project_name, n=n, **kwargs)
+    vcf_fileinfo = None
+    if not n and include_vcfs:
+        all_vcf_files = samples.download_vcf_files(project_name=project_name, **kwargs)
+        vcf_fileinfo = _merge_filepath_with_fileinfo(all_vcf_files)
     patients = []
     for (i, row) in clin.iterrows():
         if include_vcfs:
-            vcf_files = samples.download_vcf_files(query_args={'cases.case_id': row['case_id']}, project_name=project_name)
+            if vcf_fileinfo is None:
+                vcf_files = samples.download_vcf_files(query_args={'cases.case_id': row['case_id']}, project_name=project_name)
+            else:
+                vcf_files = vcf_fileinfo.loc[vcf_fileinfo['case_id']==row['case_id'], 'file_path']
             patients.append(prep_patient_data(row, snv_vcf_paths=vcf_files))
         else:
             patients.append(prep_patient_data(row))

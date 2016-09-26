42163b30f9eb19668db04472b14dfc2edad9352b
Jacqueline Buros Novik <jackinovik@gmail.com>
2016-09-26 13:41:28 -0400

    rework clin-data for new location of query_tcga

--- refresh_clinical_data.py
+++ refresh_clinical_data.py
@@ -1,7 +1,12 @@
 from query_tcga import query_tcga as qt
+from query_tcga import config
 import logging
 
-logging.basicConfig(level=logging.INFO)
+config.set_value(USE_CACHE=True)
+config.set_value(GDC_TOKEN_PATH='/Users/jacquelineburos/Downloads/gdc-user-token.2016-09-26T12-23-27-04-00.txt')
+config.set_value(GDC_DATA_DIR='/Users/jacquelineburos/projects/tcga-blca/data/gdc/')
+
+logging.basicConfig(level=logging.DEBUG)
 
 def refresh_clin_data(project_name, filename):
 	clin = qt.get_clinical_data(project_name=project_name)

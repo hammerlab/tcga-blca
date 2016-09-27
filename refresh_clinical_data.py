1e438e319bc6b2e3c134433f6e64e7bf641c71c0
Jacqueline Buros Novik <jackinovik@gmail.com>
2016-09-27 00:34:34 -0400

    clean up refresh_clinical_data script

--- refresh_clinical_data.py
+++ refresh_clinical_data.py
@@ -11,8 +11,8 @@ logging.basicConfig(level=logging.DEBUG)
 def refresh_clin_data(project_name, filename):
 	clin = qt.get_clinical_data(project_name=project_name)
 	clin.to_csv(filename, sep='|', index=False)
+	print("Clinical data written to {file} for project {project}".format(file=filename, project=project_name))
 
 if __name__ == '__main__':
 	refresh_clin_data(project_name='TCGA-BLCA', filename='data/clinical.csv')
-	print("Clinical data written to {file} for project {project}".format(file='data/clinical.csv', project='TCGA-BLCA'))
 
